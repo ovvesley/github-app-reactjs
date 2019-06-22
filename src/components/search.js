f478f4d78a8ed02724c089708b33455b13b0aedb
Wesley Ferreira <wsf.ley@gmail.com>
2019-06-22 15:02:00 -0300

    request api.github

--- src/components/search.js
+++ src/components/search.js
@@ -1,12 +1,24 @@
 'use strict'
 
 import React from 'react'
-
+import ajax from '@fdaciuk/ajax'
 const Search = () => (
     <div className='search'>
         <input
             type='search'
-            placeholder='Digite o nome do usuario' />
+            placeholder='Digite o nome do usuario'
+            onKeyUp={(e) => {
+                const value = e.target.value
+                const keyCode = e.which || e.keyCode
+                const ENTER = 13
+                if (keyCode === ENTER) {
+                    console.log(value)
+                    ajax().get(`https://api.github.com/users/${value}`)
+                        .then((result) => {
+                            console.log(result)
+                        })
+                }
+            }} />
     </div>
 )
 
