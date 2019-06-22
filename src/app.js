6542e563861638682ccbf17458954a323b21898f
Wesley Ferreira <wsf.ley@gmail.com>
2019-06-22 01:24:11 -0300

    UserInfo implement components

--- src/app.js
+++ src/app.js
@@ -1,22 +1,11 @@
 'use strict'
 import React, { Component } from 'react'
 import Search from './components/search'
+import UserInfo from './components/user-info'
 const App = () => (
     <div className='app'>
         <Search />
-        <div className='user-info'>
-            <img src='https://avatars2.githubusercontent.com/u/12123871?v=4' />
-            <h1 className='username'>
-                <a href="https://vvesly.github.io"> Wesley Ferreira </a>
-            </h1>
-
-            <ul className='repos-info'>
-                <li>- Repositorios: 0</li>
-                <li>- Seguidores: 0</li>
-                <li>- Repositorios: 0</li>
-            </ul>
-
-        </div>
+        <UserInfo />
 
         <div className="actions">
             <button> Ver Repositorios</button>
---
+++ src/components/user-info.js
@@ -0,0 +1,24 @@
+'use strict'
+
+'use strict'
+
+import React from 'react'
+
+const UserInfo = () => (
+    <div className='user-info'>
+        <img src='https://avatars2.githubusercontent.com/u/12123871?v=4' />
+        <h1 className='username'>
+            <a href="https://vvesly.github.io"> Wesley Ferreira </a>
+        </h1>
+
+        <ul className='repos-info'>
+            <li>- Repositorios: 0</li>
+            <li>- Seguidores: 0</li>
+            <li>- Repositorios: 0</li>
+        </ul>
+
+    </div>
+)
+
+
+export default UserInfo
